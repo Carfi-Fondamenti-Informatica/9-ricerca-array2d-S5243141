e953364f6593b6dd6439c065f7c7d41b428bc434
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-05-15 14:39:12 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,16 +1,16 @@
 #include "lib.h"
 
-void init (char a[10][20]) {
+void init (char mat[10][20]) {
   for (int i=0; i<10; i++) {
     for (int j=0; j<20; j++) {
-      a[i][j]=0;
+      mat[i][j]=0;
     }
   }
 }
 
-void init (char b[20]) {
+void init (char arr[20]) {
   for (int i=0; i<20; i++) {
-    b[i]=0;
+    arr[i]=0;
   }
 }
 
