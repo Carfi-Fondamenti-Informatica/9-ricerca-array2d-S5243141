ab5835bebf7056714819a24671854168ec41e9f2
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-05-15 14:27:53 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,33 @@
+#include "lib.h"
 
+void init (char a[10][20]) {
+  for (int i=0; i<10; i++) {
+    for (int j=0; j<20; j++) {
+      a[i][j]=0;
+    }
+  }
+}
+
+void init (char b[20]) {
+  for (int i=0; i<20; i++) {
+    b[i]=0;
+  }
+}
+
+int ricerca (char nome[10][20], char n[20]) {
+  int x;
+  for (int i=0; i<10; i++) {
+    x=0;
+    for (int j=0; j<20; j++) {
+      if (nome[i][j]==n[j]) {
+        x++;
+      } else {
+        break;
+      }
+    }
+    if (x==20) {
+      return i;
+    }
+  }
+  return -1;
+}
