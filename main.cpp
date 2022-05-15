bd72670e2c4e786b587deddd3fa935fcc6db3e03
S5243141 <99831210+S5243141@users.noreply.github.com>
2022-05-15 14:33:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
+int main() {
+  char nome[10][20];
+  char n[20];
+  init(nome);
+  init(n);
+  
+  for (int i=0; i<10; i++) {
+    cin >> nome[i];
+  }
+  
+  cin >>n;
+  
+  int c=ricerca(nome, n);
+  if (a!=-1) {
+    cout << a;
+  } else {
+    cout << "non presente" <<endl,
+  }
+  
   return 0;
 }
